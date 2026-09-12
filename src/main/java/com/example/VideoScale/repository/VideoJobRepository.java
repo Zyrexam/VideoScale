afbb6740de3ac0b85a408c9082c8207af817eb5a
Zyrexam <mohitkumar4922251@gmail.com>
2026-09-12 19:22:57 +0530

    refactor: drop redundant @Repository from VideoJobRepository

--- src/main/java/com/example/VideoScale/repository/VideoJobRepository.java
+++ src/main/java/com/example/VideoScale/repository/VideoJobRepository.java
@@ -2,11 +2,9 @@ package com.example.VideoScale.repository;
 
 import com.example.VideoScale.entity.VideoJob;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface VideoJobRepository extends JpaRepository<VideoJob, String> {
 
     Optional<VideoJob> findByJobId(String jobId);
